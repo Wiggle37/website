46883cb0e2d56c36433f3b1c7e793cfc964a9e45
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-21 09:56:19 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+import './App.css';
+import Home from './Home/Home'
 
+function App() {
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home></Home>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
+}
