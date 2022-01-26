c15a26bc930d9a273cf954c194dafd0bd439ebb7
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-25 22:20:24 -0500

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Header from './Header/Header';
 import Home from './Home/Home';
 import About from './About/About';
 import NotFound from './NotFound/NotFound';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-      <Header items={[
-        { name: 'Home', path: '/' },
-        { name: 'About', path: '/about' }
-      ]}></Header>
+        <Header items={[
+          { name: 'Home', path: '/' },
+          { name: 'About', path: '/about' }
+        ]}></Header>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
+        <Footer></Footer>
       </BrowserRouter>
     </div>
   );
