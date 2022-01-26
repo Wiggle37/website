6ccafd97fa238e5787f5058b04bedcac4b40388e
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-25 22:22:02 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-import Header from './Header/Header';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import Home from './Home/Home';
 import About from './About/About';
 import NotFound from './NotFound/NotFound';
-import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
