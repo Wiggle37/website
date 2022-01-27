6cd7ad12748611ed03fd4bf3c219b0a8bd49d347
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-26 21:46:51 -0500

    fix not found button

--- src/NotFound/NotFound.jsx
+++ src/NotFound/NotFound.jsx
@@ -5,8 +5,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
     return (
         <div className='NotFound'>
-            <h1>404 Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+            <h1>404 Not Found</h1>           
             <Link to='/'>
                 <button>
                     Go Back To Home
