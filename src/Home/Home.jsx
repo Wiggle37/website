ae4fb1cd0c6be8fbc94685d87956dd1aceacd4f6
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-30 00:35:42 -0500

    Add random quotes to home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,9 +3,16 @@ import React from "react";
 import "./Home.css";
 
 export default function Home() {
+    const quotes = [
+        "yes",
+        "no",
+        "yabadabadoo"
+    ]
+
     return (
         <div className="Home">
-            <h1>Home</h1>
+            <h1>Wiggle</h1>
+            <p>{quotes[Math.floor(Math.random() * quotes.length)]}</p>
         </div>
     );
 }
