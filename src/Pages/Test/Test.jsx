150c884324bbf97744ce0a253b45af4a62a21920
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-30 18:08:21 -0500

    update scroll bar

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -8,6 +8,69 @@ export default function Test() {
                 Somehow you got here... Anyways enjoy?
                 There isnt anything really much tbh
             </p>
+
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+            <p>k</p>
+
         </div>
     )
 }
