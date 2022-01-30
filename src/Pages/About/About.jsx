caa60aececfde45efd9d19b1935b42c27ce45735
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-30 02:31:42 -0500

    add some text to some pages

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,6 +4,9 @@ export default function About() {
     return (
         <div className="About">
             <h1 className='About'>About</h1>
+            <p>
+                This will be stuff about me when i get time to right it anyways
+            </p>
         </div>
     )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,6 +13,10 @@ export default function Home() {
         <div className="Home">
             <h1>Wiggle</h1>
             <p>{quotes[Math.floor(Math.random() * quotes.length)]}</p>
+
+            <p>
+                welcome to my website
+            </p>
         </div>
     );
 }
