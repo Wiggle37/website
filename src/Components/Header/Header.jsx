fe21415d5aa27fbd371961bf099d8fd24348629d
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-30 00:10:26 -0500

    fix eslint suggestions

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,14 +3,12 @@ import React from "react";
 import "./Header.css"
 
 export default function Header(props) {
-    // eslint-disable-next-line react/prop-types
     const navigation = [...props.items]
   
     return (
         <div className="Header">
             {
                 navigation.map((item) => (
-                    // eslint-disable-next-line react/jsx-key
                     <a className="left" href={item.path}>
                         {item.name}
                     </a>
