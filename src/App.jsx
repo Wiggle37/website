12343e3216d09f579c8299ef657024e615de8ad4
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-30 02:30:33 -0500

    move to pages folder, organize go br

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import Home from "./Home/Home";
-import About from "./About/About";
-import Terms from "./Terms/Terms"
-import NotFound from "./NotFound/NotFound";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Terms from "./Pages/Terms/Terms"
+import NotFound from "./Pages/NotFound/NotFound";
 
 function App() {
     return (
