159f3bf8c4b5c62ecf291337f6d8143d75c3c1ab
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-01-31 12:03:06 -0500

    About page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,12 +1,25 @@
 import React from "react"
 
+import "./About.css"
+
 export default function About() {
     return (
         <div className="About">
-            <h1 className='About'>About</h1>
-            <p>
-                This will be stuff about me when i get time to right it anyways
-            </p>
+            <h1>Hello! I'm Wiggle</h1>
+            <body>
+                <p>I enjoy programming in my freetime and playing video games.</p>
+                <p>I am a self taught programmer and I am currently learning Java and JavaScript/Typescript</p>
+                <p>Some of my hobbies are: 
+                    <ul>
+                        <li>Playing video games</li>
+                        <li>Coding</li>
+                        <li>Sleeping</li>
+                        <li>Spending time with my dog</li>
+                    </ul>
+                </p>
+
+                <p>You can contact me <a href="/contact">"here"</a></p>
+            </body>
         </div>
     )
 }
