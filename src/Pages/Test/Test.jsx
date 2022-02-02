8d7e175682fe698886a0b6d1a00b514a4df342a7
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-02-01 23:45:10 -0500

    Update Test.jsx

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -3,20 +3,6 @@ import React from "react";
 import './Test.css';
 
 export default function Test() {
-    let _ud = "";
-    async function userData() {
-        _ud = await fetch("http://localhost:8080/accounts/getAccount/matthew.t.rose.2008%40outlook.com", {
-            method: "GET"
-        }).then(
-            req => req.json()
-        );
-
-        var ele = document.createElement("p");
-        var text = document.createTextNode(`e: ${JSON.stringify(_ud)}`);
-        ele.appendChild(text);
-        var element = document.getElementById("ook");
-        element.appendChild(ele);
-    }
 
     return (
         <div className="Test" id="ook">
