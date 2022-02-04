d3ef7245bbbb8af16bb42009a349e860f3e66041
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-02-04 00:02:38 -0500

    login stuff

--- src/Pages/Accounts/Login/Login.jsx
+++ src/Pages/Accounts/Login/Login.jsx
@@ -3,27 +3,20 @@ import React from "react";
 import "./Login.css";
 
 export default function Login() {
-    async function Login() {
-        const tag = document.createElement("p");
-        const text = document.createTextNode("Login not found, please create an account");
-        tag.appendChild(text)
-        const element = document.getElementsByClassName("loginBox");
-        element.appendChild(tag)
-    }
-
     return (
         <div className="Login">
             <h1 className="title">Login</h1>
             <div className="loginBox">
                 <form>
                     <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" placeholder="Email" />
-                    <br />
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" placeholder="Password" />
+                    <input type="email" name="email" placeholder="Email" required/>
                     <br />
 
-                    <button className="submitButton" onClick={() => Login()} type="submit">Submit</button>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" placeholder="Password" required/>
+                    <br />
+
+                    <button className="submitButton" type="submit">Submit</button>
                 </form>
             </div>
         </div>
