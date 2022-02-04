5e99973f18366dd8720895bc886fd7982282270f
Wiggle <79219264+WiggleGiggle@users.noreply.github.com>
2022-02-03 23:28:48 -0500

    start on login page

--- src/Pages/Accounts/Login/Login.jsx
+++ src/Pages/Accounts/Login/Login.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
+import "./Login.css";
+
 export default function Login() {
+    async function Login() {
+        const tag = document.createElement("p");
+        const text = document.createTextNode("Login not found, please create an account");
+        tag.appendChild(text)
+        const element = document.getElementsByClassName("loginBox");
+        element.appendChild(tag)
+    }
+
     return (
         <div className="Login">
-            <p>
-                Coming Soon...
-            </p>
+            <h1 className="title">Login</h1>
+            <div className="loginBox">
+                <form action={() => Login()}>
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" placeholder="Email" required/>
+                    <br />
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" name="password" placeholder="Password" required/>
+                    <br />
+
+                    <input type="submit" />
+                </form>
+            </div>
         </div>
     )
 }
